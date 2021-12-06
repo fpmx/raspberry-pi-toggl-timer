d162621e76aa0289af8ab1ecc78fdd36d5a59e21
Fredy Peralta <44048046+fpmx@users.noreply.github.com>
2021-12-05 16:45:18 -0800

    Update config.php
    Sorry for messing around, is my first commit and had some issues doing it (not sure if is done properly, though).
    So the main change here is I added back the "$url" that was missing after the last update in Jan 2019 and added the new API address from toggl as they changed the API at the end of June 2021.
    This makes the system work again. 
    
    Many thanks for this contribution. It has been really helpful to me for the last few years.

--- config.php
+++ config.php
@@ -4,6 +4,9 @@
 
 $api_token = "Your API token here";
 
+// url required to run. API address changed at the end of June 2021.
+$url = "https://api.track.toggl.com/api/v8/time_entries/current";
+
 //-------------------------------------------------------------------
 //
 
